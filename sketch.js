1a19dc1105cbcdff1a999ceec9e881e3f94be4da
manuellyinacio <nuribeiro00@gmail.com>
2024-08-19 17:10:08 -0300

    arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+    background("pink");
+  
+  }
+  function draw() {
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,40,70);
+    
+  }
+    
+    
+  stroke("blue")
+  fill("purple")
+  
+  }
